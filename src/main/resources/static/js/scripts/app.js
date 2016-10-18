0d06131bf02574f1d48fd05a5d4b92a73422493f
Wilhelm Perdahl <wilhelm.perdahl@gmail.com>
2016-10-18 13:16:31 +0200

    added form that returns a lightweight JSON

---
+++ src/main/resources/static/js/scripts/app.js
@@ -0,0 +1,10 @@
+angular.module("academy", ['ngRoute', 'academy.form'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/form', {
+                templateUrl: 'js/scripts/form/form.template.html',
+                controller: academy.form.FormController,
+                controllerAs: 'vm'
+            })
+            .otherwise('/form')
+    }]);
