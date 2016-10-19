cfd5fdd26eea19ed11c83159565f785b1fa1f63a
Kevin <kevosclun@gmail.com>
2016-10-19 21:31:37 +0200

    Kevin Rulez

--- src/main/resources/static/js/scripts/form/form.controller.js
+++ src/main/resources/static/js/scripts/form/form.controller.js
@@ -5,22 +5,10 @@ if(!academy.form) {
     academy.form = {};
 }
 academy.form.FormController = function($http) {
-    var self = this;
+    var self =this;
     self.send = function(data) {
         console.log(data);
-        var myData = {}
-
-        for (var key in data) {
-            if (!(typeof data[key] === "undefined")) {
-                if (!(typeof data[key].$name === "undefined")) {
-                    myData[key] = data[key].$name;
-                    myData[key] = data[key].$modelValue;
-                }
-            }
-        }
-
-        console.log(myData);
-        $http.post("/form/data", myData);
+        $http.post("/form/data", data);
 
     }
 };
