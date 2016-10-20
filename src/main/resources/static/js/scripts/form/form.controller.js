8be4650ff6876d091a6d00baedf4c84e296edecd
Kevin <kevosclun@gmail.com>
2016-10-20 12:23:04 +0200

    REfactored the formcontroller in angular. added services

--- src/main/resources/static/js/scripts/form/form.controller.js
+++ src/main/resources/static/js/scripts/form/form.controller.js
@@ -4,7 +4,7 @@ if(!academy) {
 if(!academy.form) {
     academy.form = {};
 }
-//refactor till service och serviceprovider create service
+
 academy.form.FormController = function(formService) {
     this.send = function(data) {
         formService.send(data)
