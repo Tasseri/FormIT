1ab513d6e15f241bd8034d62cbc61adaff9cdf8c
Shadan Sev <shadan.sevinik@outlook.com>
2016-10-21 15:24:35 +0200

    scopechanges

--- src/main/resources/static/js/scripts/form/form.controller.js
+++ src/main/resources/static/js/scripts/form/form.controller.js
@@ -10,4 +10,113 @@ academy.form.FormController = function(formService) {
         formService.send(data)
 
     }
+    var self=this;
+    self.questions={};
+    self.questions.radio = [
+        {
+            "orderNumber": 1,
+            "questiondescr": "Är du okej?",
+            "choices": [
+                {
+                    "option": "Ja"
+                },
+                {
+                    "option": "Nej"
+                },
+                {
+                    "option": "Kanske",
+                }
+            ],
+            "answer": ''
+        },
+        {
+            "orderNumber": 2,
+            "questiondescr": "Är du hungrig?",
+            "choices": [
+                {
+                    "option": "Nej"
+                },
+                {
+                    "option": "Ja",
+                },
+                {
+                    "option": "Alltid"
+                }
+            ],
+            "answer": ''
+        },
+        {
+            "orderNumber": 3,
+            "questiondescr": "Skulle det vara bra med en \"add question\" metod?",
+            "choices": [
+                {
+                    "option": "Japp"
+                },
+                {
+                    "option": "Aldrig"
+                },
+                {
+                    "option": "Varför"
+                }
+            ],
+            "answer": ''
+        },
+        {
+            "orderNumber": 4,
+            "questiondescr": "Här är en extra fråga?",
+            "choices": [
+                {
+                    "option": "Kul"
+                },
+                {
+                    "option": "Okej vi förstår"
+                },
+                {
+                    "option": "En till tack"
+                }
+            ],
+            "answer": ''
+        },
+        {
+            "orderNumber": 5,
+            "questiondescr": "Fråga 5?",
+            "choices": [
+                {
+                    "option": "Ok",
+                },
+                {
+                    "option": "Hej"
+                },
+                {
+                    "option": "På"
+                },
+                {
+                    "option": "Dig"
+                }
+            ],
+            "answer": ''
+        },
+        {
+            "orderNumber": 6,
+            "questiondescr": "Sista frågan?",
+            "choices": [
+                {
+                    "option": "Äntligen"
+                },
+                {
+                    "option": "Då får de räcka"
+                },
+                {
+                    "option": "Några fler alternativ"
+                },
+                {
+                    "option":"Som det här"
+                },
+                {
+                    "option":"och det här"
+                }
+            ],
+            "answer": ''
+        }
+    ]
 };
