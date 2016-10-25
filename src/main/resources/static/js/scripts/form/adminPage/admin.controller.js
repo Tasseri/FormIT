546551d3c34a94d9fae5219e797ec8b9519ab14f
Kim <kim_caprett@hotmail.com>
2016-10-25 14:53:37 +0200

    Tillagd struktur (checkboxes, lista)

--- src/main/resources/static/js/scripts/form/adminPage/admin.controller.js
+++ src/main/resources/static/js/scripts/form/adminPage/admin.controller.js
@@ -6,44 +6,27 @@ if(!academy.admin) {
 }
 
 academy.admin.AdminController = function(adminService) {
-    this.send = function () {
-        adminService.send()
-
-    };
-    var self = this;
-    self.forma = adminService.getForm();
-
-    this.add = function(data){
-
-        adminService.addTextQuestion(data);
-
-    }
 
 
     this.send = function () {
         adminService.send()
-
     };
+
     var self = this;
     self.forma = adminService.getForm();
 
     this.addr = function(data){
-
         adminService.addRadioQuestion(data);
-
     }
 
     this.send = function () {
         adminService.send()
-
     };
 
-
-
     this.addChoice = function(data){
-
         adminService.addNewChoice(data);
 
     };
+
 }
 
